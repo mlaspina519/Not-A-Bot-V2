35eeb6d728cf8c2210be6bb44fe617ee1b402367
Matt <mlaspina519@gmail.com>
2018-07-15 20:07:38 -0400

    Testing audio commands

--- src/main/java/commands/Music/AudioCommand.java
+++ src/main/java/commands/Music/AudioCommand.java
@@ -1,5 +1,10 @@
 package commands.Music;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+
 public class AudioCommand {
 
+    AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
 }
